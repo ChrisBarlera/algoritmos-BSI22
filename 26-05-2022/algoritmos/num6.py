5c651cdfcd8c642c25996481ab352bdfeacabd09
Christian Barlera <christian.barlera2@gmail.com>
2022-05-26 17:48:57 -0300

    Corrigindo algoritmo de data (num6 26-05-2022)

--- 26-05-2022/algoritmos/num6.py
+++ 26-05-2022/algoritmos/num6.py
@@ -1,3 +1,7 @@
+#O usuário sempre informará uma data válida, não sendo necessário fazer este teste;
+#Considere que os anos são todos não-bissextos
+#Considere que todo mes tem 30 dias
+
 print("Informe uma data inicial")
 dia = int(input("Dia: "))
 mes = int(input("Mes: "))
@@ -10,34 +14,41 @@ if diasPassados >= 365:
     ano = ano + somaAnos
     diasPassados = diasPassados % 365
 
-if diasPassados >= 28:
-    if mes == 2:
-        qtd_DiasdoMes = 28
-        if dia+diasPassados <= qtd_DiasdoMes:
-            dia = dia + diasPassados
-        else:
-            dia = (dia + diasPassados) - qtd_DiasdoMes
-            mes = mes + 1
+if(diasPassados >= 30):
+    somaMes = diasPassados // 30
+    mes = mes + somaMes
+    diasPassados = diasPassados % 30
 
-    elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
-        qtd_DiasdoMes = 31
-        if dia+diasPassados <= qtd_DiasdoMes:
-            dia = dia + diasPassados
-        else:
-            if mes == 12:
-                dia = (dia + diasPassados) - qtd_DiasdoMes
-                mes = 1
-                ano = ano + 1
-            else: 
-                dia = (dia + diasPassados) - qtd_DiasdoMes
-                mes = mes + 1
+
+# ///////// CODIGO ABANDONADO (TENTATIVA DE CONSIDERAR A QUANTIDADE DE DIAS CERTOS DE CADA MES)
+# if diasPassados >= 28:
+#     if mes == 2:
+#         qtd_DiasdoMes = 28
+#         if dia+diasPassados <= qtd_DiasdoMes:
+#             dia = dia + diasPassados
+#         else:
+#             dia = (dia + diasPassados) - qtd_DiasdoMes
+#             mes = mes + 1
+
+#     elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+#         qtd_DiasdoMes = 31
+#         if dia+diasPassados <= qtd_DiasdoMes:
+#             dia = dia + diasPassados
+#         else:
+#             if mes == 12:
+#                 dia = (dia + diasPassados) - qtd_DiasdoMes
+#                 mes = 1
+#                 ano = ano + 1
+#             else: 
+#                 dia = (dia + diasPassados) - qtd_DiasdoMes
+#                 mes = mes + 1
                 
-    else:
-        qtd_DiasdoMes = 30
-        if dia+diasPassados <= qtd_DiasdoMes:
-            dia = dia + diasPassados
-        else:
-            dia = (dia + diasPassados) - qtd_DiasdoMes
-            mes = mes + 1
+#     else:
+#         qtd_DiasdoMes = 30
+#         if dia+diasPassados <= qtd_DiasdoMes:
+#             dia = dia + diasPassados
+#         else:
+#             dia = (dia + diasPassados) - qtd_DiasdoMes
+#             mes = mes + 1
 
-print(f"Nova data:{dia,mes,ano}")
+print(f"Nova data:{dia+diasPassados,mes,ano}")
