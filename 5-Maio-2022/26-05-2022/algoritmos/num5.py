ad7d6ae24e9dd31422fdf129ecbb049a27969e75
Christian <christian.barlera2@gmail.com>
2022-05-28 23:46:57 -0300

    Finalizando exercicio 26-05-2022

--- 5-Maio-2022/26-05-2022/algoritmos/num5.py
+++ 5-Maio-2022/26-05-2022/algoritmos/num5.py
@@ -4,11 +4,20 @@ tipo = input("Tipo de combustível(A ou G): ")
 precoAlcool = 6.5
 precoGasolina = 7.2
 if tipo == "A":
-    valorInicial = precoAlcool * litros
-    if litros < 20:
-        desconto = valorInicial * 0.03
-        valorFinal = valorInicial - desconto
-        print(valorFinal)
-else :
-    valorInicial = precoGasolina * litros
-    
+    if litros <= 20:
+        # desconto de 3% por litro
+        resultado = (precoAlcool * 0.97) * litros
+        print("Valor a ser pago (com desconto):",resultado)
+    else:
+        # desconto de 5% por litro
+        resultado = (precoAlcool * 0.95) * litros
+        print("Valor a ser pago (com desconto):",resultado)
+else:
+    if litros <= 20:
+        # desconto de 4% por litro
+        resultado = (precoGasolina * 0.96) * litros
+        print("Valor a ser pago (com desconto):",resultado)
+    else:
+        # desconto de 6% por litro
+        resultado = (precoGasolina * 0.94) * litros
+        print("Valor a ser pago (com desconto):",resultado)
