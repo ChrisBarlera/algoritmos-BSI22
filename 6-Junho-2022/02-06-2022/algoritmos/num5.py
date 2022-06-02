4f95d616dcd9ed32125b83fe74007ddb3d6d678a
Christian Barlera <christian.barlera2@gmail.com>
2022-06-02 10:10:27 -0300

    Add files via upload

---
+++ 6-Junho-2022/02-06-2022/algoritmos/num5.py
@@ -0,0 +1,23 @@
+tipo = input("Tipo de combustível(A ou G): ")
+litros = float(input("Litros vendidos: "))
+
+precoAlcool = 6.5
+precoGasolina = 7.2
+if tipo == "A":
+    if litros <= 20:
+        # desconto de 3% por litro
+        resultado = (precoAlcool * 0.97) * litros
+        print("Valor a ser pago (com desconto):",resultado)
+    else:
+        # desconto de 5% por litro
+        resultado = (precoAlcool * 0.95) * litros
+        print("Valor a ser pago (com desconto):",resultado)
+else:
+    if litros <= 20:
+        # desconto de 4% por litro
+        resultado = (precoGasolina * 0.96) * litros
+        print("Valor a ser pago (com desconto):",resultado)
+    else:
+        # desconto de 6% por litro
+        resultado = (precoGasolina * 0.94) * litros
+        print("Valor a ser pago (com desconto):",resultado)
